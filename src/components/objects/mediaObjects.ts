fc2c1d11e672408a3f9f9f5337620f9f198b2b59
min <67621261+mino-8136@users.noreply.github.com>
2024-06-11 04:48:28 +0900

    型ファイルの定義完了

--- src/components/objects/mediaObjects.ts
+++ src/components/objects/mediaObjects.ts
@@ -1,84 +1,130 @@
-export class BaseObjectClass {
+// settingsをuserdataに移設
+
+export interface BaseSettings {
   id: number
   start: number
   end: number
   layer: number
-  overlay: number
-  camera: number
-
-  constructor(
-    id: number,
-    start: number,
-    end: number,
-    layer: number,
-    overlay: number,
-    camera: number
-  ) {
-    this.id = id
-    this.start = start
-    this.end = end
-    this.layer = layer
-    this.overlay = overlay
-    this.camera = camera
-  }
+  //overlay: number
+  //camera: number
 }
 
-export interface TextSettings {
+export interface AnimationSettings {
+  name: string
+  parameters: {}
+}
+
+export interface StandardRenderSettings {
+  X: number
+  Y: number
+  //Z: number
+  scale: number
+  transparency: number
+  rotation: number
+  //blend: number
+}
+
+export interface TextSettings extends BaseSettings, AnimationSettings, StandardRenderSettings {
+  //type: string
+  //name: string
   size: number
-  font: string
-  color: string
-  color2: string
-  text: string
-  soft: boolean
-  monospace: boolean
+  //display_speed: number
+  individual_object: boolean
+  //display_coordinates: boolean
+  //auto_scroll: boolean
+  //bold: boolean
+  //italic: boolean
+  //autoadjust: boolean
+  //soft: boolean
+  //monospace: boolean
   align: number
-  spacingX: number
-  spacingY: number
-  precision: number
-  bold: boolean
-  italic: boolean
+  spacing_x: number
+  spacing_y: number
+  //precision: number
+  color: string
+  //color2: '000000'
+  font: string
+  text: string
 }
 
-export class TextObjectClass extends BaseObjectClass {
-  settings: TextSettings
+class TextObject implements TextSettings {
+  id: number
+  start: number
+  end: number
+  layer: number
+  X: number
+  Y: number
+  scale: number
+  transparency: number
+  rotation: number
+  name: string
+  parameters: any
+  size: number
+  individual_object: boolean
+  align: number
+  spacing_x: number
+  spacing_y: number
+  color: string
+  font: string
+  text: string
 
-  constructor(
-    id: number,
-    start: number,
-    end: number,
-    layer: number,
-    overlay: number,
-    camera: number,
-    settings: TextSettings
-  ) {
-    super(id, start, end, layer, overlay, camera)
-    this.settings = settings
+  constructor(settings: TextSettings) {
+    this.id = settings.id
+    this.start = settings.start
+    this.end = settings.end
+    this.layer = settings.layer
+    this.X = settings.X
+    this.Y = settings.Y
+    this.scale = settings.scale
+    this.transparency = settings.transparency
+    this.rotation = settings.rotation
+    this.name = settings.name
+    this.parameters = settings.parameters
+    this.size = settings.size
+    this.individual_object = settings.individual_object
+    this.align = settings.align
+    this.spacing_x = settings.spacing_x
+    this.spacing_y = settings.spacing_y
+    this.color = settings.color
+    this.font = settings.font
+    this.text = settings.text
   }
 
-  // Unicode文字列をデコードするメソッド
-  decodeText(encodedText: string): string {
-    return decodeURIComponent(
-      encodedText
-        .split('')
-        .map((c) => `%${('000' + c.charCodeAt(0).toString(16)).slice(-4)}`)
-        .join('')
-    )
+  render(): void {
+    // ここでテキストオブジェクトのレンダリングロジックを実装する
   }
 }
 
-export class ImageObjectClass extends BaseObjectClass {
+export interface ImageSettings extends BaseSettings, AnimationSettings, StandardRenderSettings {
   file: string
+}
 
-  constructor(
-    id: number,
-    start: number,
-    end: number,
-    layer: number,
-    overlay: number,
-    camera: number,
-    file: string
-  ) {
-    super(id, start, end, layer, overlay, camera)
-    this.file = file // 画像ファイルのパス
+class ImageObject implements ImageSettings {
+  id: number
+  start: number
+  end: number
+  layer: number
+  X: number
+  Y: number
+  scale: number
+  transparency: number
+  rotation: number
+  file: string
+  name: string
+  parameters: any
+
+  constructor(settings: ImageSettings) {
+    this.id = settings.id
+    this.start = settings.start
+    this.end = settings.end
+    this.layer = settings.layer
+    this.X = settings.X
+    this.Y = settings.Y
+    this.scale = settings.scale
+    this.transparency = settings.transparency
+    this.rotation = settings.rotation
+    this.file = settings.file
+    this.name = settings.name
+    this.parameters = settings.parameters
   }
 }
