07816f583e54494ebd24e3216bd3b2b7efbb8c59
mino-8136 <67621261+mino-8136@users.noreply.github.com>
2024-07-02 19:50:14 +0900

    ボタンをタブに変更

--- src/components/editor/PreviewPanel.vue
+++ src/components/editor/PreviewPanel.vue
@@ -1,15 +1,26 @@
 <template>
   <v-container class="preview-panel">
-    <v-btn color="primary" @click="saveData">保存</v-btn>
-    <v-btn color="secondary" @click="loadData">読み込み</v-btn>
-    <div class="preview-area">
+    <v-menu>
+      <template v-slot:activator="{ props }">
+        <v-btn color="primary" v-bind="props" density="comfortable" > ファイル </v-btn>
+      </template>
 
+      <v-list>
+        <v-list-item @click="openFile">
+          <v-list-item-title>ファイルを開く</v-list-item-title>
+        </v-list-item>
+        <v-list-item @click="saveFile">
+          <v-list-item-title>ファイルを保存</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-menu>
+
+    <div class="preview-area">
       <!-- ここでオブジェクトのコンポーネントを表示 -->
       <div v-for="object in props.objects" :key="object.id">
-        <component :is="getObjectComponent(object.type)" /> 
+        <component :is="getObjectComponent(object.type)" />
         <!-- TODO : 表示する位置を指定できるようにする-->
       </div>
-
     </div>
   </v-container>
 </template>
@@ -18,22 +29,22 @@
 import { computed, ref, defineProps } from 'vue'
 
 // TimelinePanel上で現在再生されているオブジェクトを取得
-const props = defineProps(['objects']); // EditViewから渡されたCurrentObjects
-
+const props = defineProps(['objects']) // EditViewから渡されたCurrentObjects
 
 function getObjectComponent(type: string) {
   if (type === 'text') return 'TextObject'
   if (type === 'image') return 'ImageObject'
 }
 
-
-const saveData = () => {
+const saveFile = () => {
   // TODO: データを保存する処理を追加
 }
 
-const loadData = () => {
+const openFile = () => {
   // TODO: データを読み込む処理を追加
 }
+
+
 </script>
 
 <style scoped>
