fac7961b3968bffa1748a04233dc3f2254ecf00b
mino-8136 <67621261+mino-8136@users.noreply.github.com>
2024-07-02 18:52:48 +0900

    ファイル構造の更新

---
+++ src/components/editor/SettingPanel.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-container class="setting-panel">
+    <div v-if="selectedObject">
+      <!-- 選択されたオブジェクトの種類に基づいてUIを表示 -->
+
+      <div v-if="selectedObject.type === 'text'">
+        <label>テキスト:</label>
+        <input v-model="selectedObject.text" type="text" />
+      </div>
+
+      <div v-if="selectedObject.type === 'image'">
+        <label>画像URL:</label>
+        <input v-model="selectedObject.url" type="text" />
+      </div>
+      
+    </div>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// TODO : 仮で入れているだけ
+const selectedObject = ref({
+  type: 'text',
+  text: 'Hello, World!'
+})
+</script>
+
+<style scoped>
+.setting-panel {
+  padding: 10px;
+  border: 1px solid #ccc;
+  position: relative;
+  width: 100%;
+  height: 500px;
+}
+
+</style>
