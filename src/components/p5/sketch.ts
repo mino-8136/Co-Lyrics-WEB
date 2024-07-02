2c9c09ec9977137fb1a46c27a17dbd1f70181df1
mino-8136 <67621261+mino-8136@users.noreply.github.com>
2024-07-02 20:39:16 +0900

    letをconstに

--- src/components/p5/sketch.ts
+++ src/components/p5/sketch.ts
@@ -1,7 +1,7 @@
 // src/components/p5/sketch.js
 export default function sketch(p) {
   p.setup = () => {
-    let canvas = p.createCanvas(400, 400);
+    const canvas = p.createCanvas(400, 400);
     canvas.parent('p5Canvas');
     p.background(0);
   };
