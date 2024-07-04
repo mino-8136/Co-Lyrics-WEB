2e6a70db8fa8b782f56f3cf4ea7b897d4c047bb8
min <67621261+mino-8136@users.noreply.github.com>
2024-07-04 17:28:01 +0900

    注釈付与

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import './assets/main.css'
 
+// Vue
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-
 import App from './App.vue'
 import router from './router'
 
@@ -12,14 +12,13 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 import "@mdi/font/css/materialdesignicons.css";
-
 const vuetify = createVuetify({
   components,
   directives
 })
 
+// Create the app
 const app = createApp(App)
-
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
