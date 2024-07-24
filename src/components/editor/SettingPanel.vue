93bdf18ce8134d764ba25252e0f6e845f19dd7a6
min <67621261+mino-8136@users.noreply.github.com>
2024-07-25 01:51:31 +0900

    パネル表示の作成

--- src/components/editor/SettingPanel.vue
+++ src/components/editor/SettingPanel.vue
@@ -2,45 +2,67 @@
   <v-container class="setting-panel">
     <div v-if="selectedObject">
       <!-- 選択されたオブジェクトの種類に基づいてUIを表示 -->
-
-      <div v-if="selectedObject.type === 'text'">
-
-        <textarea v-model="selectedObject.text" type="text" />
-
-        <v-slider label="val1"></v-slider>
-        <v-slider label="val1"></v-slider>
-        <v-slider label="val1"></v-slider>
-        <v-slider label="val1"></v-slider>
+      <div v-for="(value, key) in selectedObject" :key="key" class="parameter-row">
+        <template v-if="isNumber(value)">
+          <div class="parameter-label">{{ key }}:</div>
+          <div class="parameter-value">{{ value }}</div>
+          <v-slider
+            v-model="selectedObject[key]"
+            :min="0"
+            :max="100"
+          ></v-slider>
+        </template>
+        <template v-else-if="isString(value)">
+          <div class="parameter-label">{{ key }}:</div>
+          <input :id="key" v-model="selectedObject[key]" type="text" />
+        </template>
       </div>
-
-      <div v-if="selectedObject.type === 'image'">
-        <label>画像URL:</label>
-        <input v-model="selectedObject.url" type="text" />
-      </div>
-      
     </div>
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { ref, computed } from 'vue'
+import { computed } from 'vue'
 import { useObjectStore } from '@/stores/objectStore'
 const objectStore = useObjectStore()
 
-// 選択されたオブジェクトの情報
+// 選択されたオブジェクトの情報が自動的に表示される
 const selectedObject = computed(() => {
   return objectStore.objects.find(obj => obj.selected)
 })
 
+// 型チェック関数
+const isNumber = (value: unknown): value is number => typeof value === 'number'
+const isString = (value: unknown): value is string => typeof value === 'string'
 </script>
 
 <style scoped>
+
 .setting-panel {
   width: 100%;
   height: 500px;
   padding: 10px;
   border: 1px solid #ccc;
-  overflow-y: auto
+  overflow-y: auto;
+}
+
+
+.parameter-row {
+  display: flex;
+  align-items: center;
+  margin-bottom: 10px;
+}
+
+.parameter-label {
+  width: 100px;
+  font-weight: bold;
+  margin-right: 10px;
+}
+
+.parameter-value {
+  width: 30px;
+  margin-left: 10px;
+  color: #555;
 }
 
 textarea {
@@ -50,5 +72,4 @@ textarea {
   box-sizing: border-box;
   border: 1px solid #ccc;
 }
-
 </style>
