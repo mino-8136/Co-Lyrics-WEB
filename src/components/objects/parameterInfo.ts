36af28350991f3a103a764b0fe8eb6c12d142377
min <67621261+mino-8136@users.noreply.github.com>
2024-07-25 19:36:01 +0900

    typeパラメータの追加

--- src/components/objects/parameterInfo.ts
+++ src/components/objects/parameterInfo.ts
@@ -6,20 +6,15 @@ export enum UIType {
   color = 'color',
   text = 'text'
 }
-// パラメータ名に対応する情報を持つオブジェクト
+// 設定パネルで表示するパラメータに対応する情報を持つオブジェクト
 export const parameterInfo: {
   [key: string]: {
     name: string
-    min: number
-    max: number
     type: UIType
+    min?: number
+    max?: number
   }
 } = {
-  id: { name: 'ID', min: 0, max: 1000, type: UIType.none },
-  start: { name: '開始', min: 0, max: 1000, type: UIType.none },
-  end: { name: '終了', min: 0, max: 1000, type: UIType.none },
-  layer: { name: 'レイヤー', min: 0, max: 10, type: UIType.none },
-  selected: { name: '選択', min: 0, max: 1, type: UIType.none },
   X: { name: 'X', min: -2048, max: 2048, type: UIType.slider },
   Y: { name: 'Y', min: -2048, max: 2048, type: UIType.slider },
   scale: { name: '拡大率', min: 0, max: 100, type: UIType.slider },
@@ -31,6 +26,6 @@ export const parameterInfo: {
   spacing_x: { name: '水平間隔', min: 0, max: 100, type: UIType.none },
   spacing_y: { name: '垂直間隔', min: 0, max: 100, type: UIType.none },
   color: { name: '色', min: 0, max: 0xffffff, type: UIType.color },
-  font: { name: 'フォント', min: 0, max: 100, type: UIType.select },
-  text: { name: 'テキスト', min: 0, max: 100, type: UIType.text }
+  font: { name: 'フォント', type: UIType.select },
+  text: { name: 'テキスト', type: UIType.text }
 }
