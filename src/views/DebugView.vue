ba7ffc510f6ee442342f4d4dfeca67e9f5dcaefc
mino-8136 <67621261+mino-8136@users.noreply.github.com>
2024-07-30 09:28:03 +0900

    GSAPテスト終了

--- src/views/DebugView.vue
+++ src/views/DebugView.vue
@@ -3,9 +3,16 @@
   <p>{{ tmp }}</p>
   <v-btn @click="animStart" icon="mdi-play"> </v-btn>
   <v-btn @click="animStart2" icon="mdi-play"> </v-btn>
-  <v-slider v-model="start" max="100" step="1"/>
-  <v-slider v-model="end" max="100" step="1"/>
-</template>
+  
+  <v-row class="ma-4">
+      <v-slider :label="`${start}`" v-model="start" max="100" step="1"/>
+      <v-slider :label="`${end}`" v-model="end" max="100" step="1"/>
+      <v-slider :label="`${animStartFrame}`" v-model="animStartFrame" max="100" step="1"/>
+      <v-slider :label="`${animEndFrame}`" v-model="animEndFrame" max="100" step="1"/>
+
+        </v-row>
+      
+    </template>
 <script setup lang="ts">
 import { ref } from 'vue'
 import { gsap } from 'gsap'
@@ -13,12 +20,15 @@ import { gsap } from 'gsap'
 const tmp = ref(0)
 const start = ref(0)
 const end = ref(0)
+const animStartFrame =ref(0)
+const animEndFrame = ref(0)
+
 
 function animStart() {
   gsap.to(tmp, { value: 100, duration: 1, ease: 'power4.out' })
 }
 function animStart2() {
-  gsap.fromTo(tmp, { value: start.value }, { value: end.value, duration: 1, ease: 'power4.out' })
+  gsap.fromTo(tmp, { value: start.value }, { value: end.value, duration: animEndFrame.value-animStartFrame.value, ease: 'power4.out' })
 }
 </script>
 
