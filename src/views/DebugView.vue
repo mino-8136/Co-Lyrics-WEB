65280183bcf87316b2908b32b3a0e22a773658ea
mino-8136 <67621261+mino-8136@users.noreply.github.com>
2024-07-30 09:16:24 +0900

    GSAPの動作テスト

--- src/views/DebugView.vue
+++ src/views/DebugView.vue
@@ -1,79 +1,25 @@
 <template>
-  <div class="ruler" @mousedown="moveSeekbar">
-    <ul class="ruler-x">
-      <li v-for="n in ticks" :key="n"></li>
-    </ul>
-  </div>
-  <div class="seekbar" :style="{ left: seekbarPosition + 'px' }"></div>
+  <h1>Debug View</h1>
+  <p>{{ tmp }}</p>
+  <v-btn @click="animStart" icon="mdi-play"> </v-btn>
+  <v-btn @click="animStart2" icon="mdi-play"> </v-btn>
+  <v-slider v-model="start" max="100" step="1"/>
+  <v-slider v-model="end" max="100" step="1"/>
 </template>
-
 <script setup lang="ts">
 import { ref } from 'vue'
-const ticks = ref(20)
-const seekbarPosition = ref(0);
+import { gsap } from 'gsap'
 
-const moveSeekbar = (event: MouseEvent) => {
-  const rulerElement = event.currentTarget as HTMLElement;
-  const rulerRect = rulerElement.getBoundingClientRect();
-  seekbarPosition.value = event.clientX - rulerRect.left;
-};
+const tmp = ref(0)
+const start = ref(0)
+const end = ref(0)
+
+function animStart() {
+  gsap.to(tmp, { value: 100, duration: 1, ease: 'power4.out' })
+}
+function animStart2() {
+  gsap.fromTo(tmp, { value: start.value }, { value: end.value, duration: 1, ease: 'power4.out' })
+}
 </script>
 
-<style scoped>
-.ruler {
-  --ruler-unit: 2px;
-  --ruler-num-fz: 10px;
-  --ruler-num-c: #888;
-
-  --ruler1-bdw: 1px;
-  --ruler1-c: #bbb;
-  --ruler1-h: 10px;
-  --ruler1-space: 5;
-
-  --ruler2-bdw: 1px;
-  --ruler2-c: #bbb;
-  --ruler2-h: 20px;
-  --ruler2-space: 50;
-
-  background-image: linear-gradient(90deg, var(--ruler1-c) 0 var(--ruler1-bdw), transparent 0),
-    linear-gradient(90deg, var(--ruler2-c) 0 var(--ruler2-bdw), transparent 0);
-  background-repeat: repeat-x;
-  background-size:
-    calc(var(--ruler-unit) * var(--ruler1-space)) var(--ruler1-h),
-    calc(var(--ruler-unit) * var(--ruler2-space)) var(--ruler2-h);
-}
-
-.ruler-x {
-  width: 100%;
-  padding: 0;
-  margin: 0;
-  color: var(--ruler-num-c);
-  font-size: var(--ruler-num-fz);
-
-  display: flex;
-  overflow: hidden;
-  line-height: calc(var(--ruler2-h) * 2 - var(--ruler-num-fz) / 2);
-  list-style: none;
-
-  counter-reset: frameCounter calc(var(--ruler2-space) * -1);
-}
-
-.ruler-x li {
-  align-self: flex-end;
-  counter-increment: frameCounter var(--ruler2-space);
-  flex: 0 0 calc(var(--ruler-unit) * var(--ruler2-space));
-}
-
-.ruler-x li::after {
-  content: counter(frameCounter);
-  line-height: 1;
-  padding-inline-start: 1.75px;
-}
-
-.seekbar {
-  width: 2px;
-  height: 100%;
-  background: #626262;
-  position: absolute;
-}
-</style>
+<style scoped></style>
