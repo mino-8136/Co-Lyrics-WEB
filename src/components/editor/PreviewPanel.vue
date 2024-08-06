caa021c2998c195b8566985546ca081a47aac422
min <67621261+mino-8136@users.noreply.github.com>
2024-08-06 11:51:56 +0900

    レンダリングするオブジェクトのON/OFFを実装

--- src/components/editor/PreviewPanel.vue
+++ src/components/editor/PreviewPanel.vue
@@ -1,32 +1,36 @@
 <template>
   <v-container class="preview-panel">
-
     <!-- ここでp5.jsのCanvasを表示 -->
     <div id="canvas"></div>
     <v-btn @click="renderObjects" icon="mdi-play"></v-btn>
-
   </v-container>
 </template>
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue'
+import { onMounted, ref, computed, watch } from 'vue'
 import { useObjectStore } from '@/stores/objectStore'
-import p5 from "p5";
-import { sketch } from '../p5/sketch';
+import p5 from 'p5'
+import { sketch } from '../p5/sketch'
 
 const objectStore = useObjectStore()
 
 // マウント時に p5.canvas を生成
-const p = ref();
+const p = ref()
 onMounted(() => {
-  p.value = new p5(sketch);
-});
+  p.value = new p5(sketch)
+})
 
-// 現在のオブジェクトをp5.canvasに登録
-function renderObjects(){
-  p.value.addRenderObjects(objectStore.objects);
+watch(
+  () => objectStore.currentFrame,
+  () => {
+    renderObjects()
+  }
+)
+
+// 現在レンダリングするオブジェクトの描画 p5.canvasに登録
+function renderObjects() {
+  p.value.addRenderObjects(objectStore.currentObjects)
 }
-
 </script>
 
 <style scoped>
