3c540ae606e76c299246b0c4f6beef3f27a106ce
min <67621261+mino-8136@users.noreply.github.com>
2024-08-11 00:39:31 +0900

    debugでの音声波形1

--- src/views/DebugView.vue
+++ src/views/DebugView.vue
@@ -1,99 +1,42 @@
 <template>
-  <h1>Debug View</h1>
-  <p>{{ animVal }}</p>
-  <v-btn @click="animStart">Timeline Start</v-btn>
-  <v-btn @click="tl.restart()">Timeline Reset</v-btn>
-  <v-btn @click="tl.pause()">Timeline Pause</v-btn>
-
-  <v-row class="ma-4">
-    <v-slider :label="`${start}`" v-model="start" max="100" step="1" />
-    <v-slider :label="`${end}`" v-model="end" max="100" step="1" />
-    <v-slider :label="`${animStartFrame}`" v-model="animStartFrame" max="5" step="1" />
-    <v-slider :label="`${animEndFrame}`" v-model="animEndFrame" max="10" step="1" />
-  </v-row>
-
-  <v-divider />
-  <div class="buttons">
-    <button @click="trackStop" title="Stop">■</button>
-    <button @click="trackPause" title="Pause">||</button>
-    <button @click="trackPlay" title="Play">▶</button>
-  </div>
-  <div class="range">
-    <input type="range" min="0" :max="100" step="0.001" v-model="currentTime" />
-  </div>
-  <div class="time">{{ timeDisplay }}sec</div>
+  <div ref="waveform"></div>
 </template>
-<script setup lang="ts">
-import { ref, computed, watchEffect } from 'vue'
-import { gsap } from 'gsap'
 
-const animVal = ref(0)
-const start = ref(0)
-const end = ref(100)
-const animStartFrame = ref(0)
-const animEndFrame = ref(5)
+<script setup>
+import { onMounted, onUnmounted, ref } from 'vue';
+import WaveSurfer from 'wavesurfer.js';
 
-// 3分のアニメーションを想定
-let tl = gsap.timeline({
-  paused: true
-})
-const currentTime = ref(0) //
-const lock = ref(false) // 操作をロックする機能
-function trackPause() {
-  gsap.killTweensOf(currentTime)
-  currentTime.value = 0
-  lock.value = false
-}
-function trackStop() {
-  gsap.killTweensOf(currentTime)
-  lock.value = false
-}
-function trackPlay() {
-  gsap.fromTo(
-    currentTime,
-    { value: 0 },
-    {
-      value: 180,
-      duration: 180,
-      ease: 'none',
-      onStart: () => {
-        lock.value = true
-      },
-      onComplete: () => {
-        lock.value = false
-      }
-    }
-  )
-}
+const waveform = ref(null); // DOM要素への参照を作成
+let wavesurfer = null;      // wavesurferのインスタンスを保持する変数
 
-watchEffect(() => {
-  tl.seek(Number(currentTime.value))
-})
+onMounted(() => {
+  if (waveform.value) {
+    // インスタンスを生成し、設定を行う
+    wavesurfer = WaveSurfer.create({
+      container: waveform.value, // refを使用して要素を指定
+      waveColor: '#4F4A85',
+      progressColor: '#383351',
+      url: '/src/assets/music/demo.mp3' // オーディオファイルのURL
+    });
 
-const timeDisplay = computed(() => Number(currentTime.value).toFixed(2))
+    // イベントリスナーを追加
+    wavesurfer.on('interaction', () => wavesurfer.play());
 
-function animStart() {
-  tl.fromTo(
-    animVal,
-    { value: start.value },
-    { value: end.value, duration: animEndFrame.value - animStartFrame.value, ease: 'power4.out' },
-    animStartFrame.value
-  )
-}
+    // オーディオのロード
+    wavesurfer.load();
+  }
+});
+
+onUnmounted(() => {
+  if (wavesurfer) {
+    // リソースのクリーンアップ
+    wavesurfer.destroy();
+  }
+});
 </script>
 
 <style scoped>
-button {
-  width: 200px;
-  height: 32px;
-  appearance: none;
-  border: 1px solid rgb(0, 0, 0);
-  color: rgb(0, 0, 0);
-  background: none;
-  user-select: none;
-}
-button:hover {
-  color: black;
-  background: rgb(181, 181, 181);
+div {
+  background-color: aliceblue;
 }
 </style>
