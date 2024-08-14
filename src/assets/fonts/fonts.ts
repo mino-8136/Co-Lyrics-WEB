d163fe5ffafad303d6e136225deefc71374e374b
min <67621261+mino-8136@users.noreply.github.com>
2024-08-15 03:26:04 +0900

    フォント一覧を構造体化

--- src/assets/fonts/fonts.ts
+++ src/assets/fonts/fonts.ts
@@ -1,8 +1,38 @@
-export const fontListData = [
-    '851チカラヅヨク',
-    'homura',
-    '瀬戸フォントSP_sjis版',
-    'SourceHanSansJP',
-    'SourceHanSerifJP',
-    'Yusei_Magic'
-  ]
+interface FontList {
+  name: string;
+  src: string;
+  format: string;
+}
+
+export const fontListData: FontList[] = [
+  {
+    name: '851チカラヅヨク',
+    src: '@/assets/fonts/851チカラヅヨク/851CHIKARA-DZUYOKU_kanaA_004.ttf',
+    format: '.ttf'
+  },
+  {
+    name: 'homura',
+    src: '@/assets/fonts/homura/03HomuraM-SemiLight.otf',
+    format: '.otf'
+  },
+  {
+    name: '瀬戸フォントSP_sjis版',
+    src: '@/assets/fonts/sjis_sp_setofont/sjis_sp_setofont.ttf',
+    format: '.ttf'
+  },
+  {
+    name: 'SourceHanSansJP',
+    src: '@/assets/fonts/SourceHanSansJP/SourceHanSansJP-Medium.otf',
+    format: '.otf'
+  },
+  {
+    name: 'SourceHanSerifJP',
+    src: '@/assets/fonts/SourceHanSerifJP/SourceHanSerifJP-Medium.otf',
+    format: '.otf'
+  },
+  {
+    name: 'Yusei_Magic',
+    src: '@/assets/fonts/Yusei_Magic/YuseiMagic-Regular.ttf',
+    format: '.ttf'
+  }
+];
