bc392bbf08093704a7747cbb9cd02f31a04282b7
min <67621261+mino-8136@users.noreply.github.com>
2024-08-18 21:58:46 +0900

    カラーパネル表示の試作

---
+++ src/views/DebugView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="text-center">
+    <v-menu v-model="colorMenu" :close-on-content-click="false" location="end">
+      <template v-slot:activator="{ props }">
+        <v-btn :color="color" v-bind="props"> {{ color }} </v-btn>
+      </template>
+      <v-color-picker v-model="color" flat />
+    </v-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+const colorMenu = ref(false)
+const color = ref('#1976D2')
+
+</script>
