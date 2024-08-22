d4dd91c1bd83e361fb9a581059df59584f12a200
min <67621261+mino-8136@users.noreply.github.com>
2024-08-22 09:17:49 +0900

    仮の保存・読み込み処理の作成

--- src/components/editor/MenuPanel.vue
+++ src/components/editor/MenuPanel.vue
@@ -9,18 +9,58 @@
         <v-list-item-title>ファイルを開く</v-list-item-title>
       </v-list-item>
       <v-list-item @click="saveFile">
-        <v-list-item-title>ファイルを保存</v-list-item-title>
+        <v-list-item-title @click="saveFile">ファイルを保存</v-list-item-title>
       </v-list-item>
     </v-list>
   </v-menu>
 </template>
 
 <script setup lang="ts">
-const saveFile = () => {
-  // TODO: storeのデータを保存する処理を追加
-}
+import { useObjectStore, useTimelineStore } from '@/stores/objectStore'
+const objectStore = useObjectStore()
 
+const saveFile = () => {
+  // ストアのデータをJSONに変換
+  let jsonData = JSON.stringify(objectStore)
+  const blob = new Blob([jsonData], { type: 'application/json' })
+  const url = URL.createObjectURL(blob)
+  const a = document.createElement('a')
+  a.href = url
+  a.download = 'data.json'  // ダウンロードするファイル名
+  
+  // リンクをクリックしてダウンロードをトリガー
+  document.body.appendChild(a)
+  a.click()
+  
+  // リンクを削除
+  document.body.removeChild(a)
+  // メモリを解放
+  URL.revokeObjectURL(url)
+}
 const openFile = () => {
   // TODO: jsonのデータを読み込む処理を追加
+  const input = document.createElement('input')
+  input.type = 'file'
+  input.accept = 'application/json'
+
+  input.onchange = (event: Event) => {
+    const file = (event.target as HTMLInputElement).files?.[0]
+    if (!file) return
+    
+    const reader = new FileReader()
+    reader.onload = () => {
+      const jsonData = reader.result as string
+      const objData = JSON.parse(jsonData)
+      
+      // ストアにデータを反映させる処理
+
+      objectStore.objects = objData.objects
+      objectStore.counter = objData.counter
+    }
+    
+    reader.readAsText(file)
+  }
+  // ファイル選択ダイアログを開く
+  input.click()
 }
 </script>
