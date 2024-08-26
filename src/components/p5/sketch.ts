5e0d3f5d01c954c2352a921756171d56318cb151
min <67621261+mino-8136@users.noreply.github.com>
2024-08-26 11:19:53 +0900

    線を非表示

--- src/components/p5/sketch.ts
+++ src/components/p5/sketch.ts
@@ -30,6 +30,7 @@ export function defineSketch(project: any) {
       p.smooth()
       p.angleMode(p.DEGREES)
       p.colorMode(p.RGB, 255, 255, 255, 100)
+      p.noStroke()
       p.rectMode(p.CENTER)
       p.frameRate(project.framerate)
 
