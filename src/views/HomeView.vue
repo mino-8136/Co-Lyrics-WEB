23c6d6abfa3d298394c13aeb5795115c7b861c18
min <67621261+mino-8136@users.noreply.github.com>
2024-08-30 07:37:02 +0900

    楽曲選択画面の追加

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,6 +13,47 @@
     </v-card-text>
   </v-card>
 
+  <v-container>
+    <v-card class="pa-4">
+      <v-card-title>楽曲情報</v-card-title>
+
+      <v-row>
+        <v-col cols="6">
+          <v-card variant="elevated" rounded="xl">
+            <template v-slot:prepend>
+              <v-icon icon="mdi-music" color="blue-lighten-3"></v-icon>
+            </template>
+            <template v-slot:title> レターポスト </template>
+            <template v-slot:subtitle> ??? feat.重音テト </template>
+            <v-card-text>
+              淡く刻んだ文字列が 君にはどんな風に見えてたの ああだこうだと捉えては
+              確証も無しに震えてる きらり色づく文字列は 私の頭を捩じ切らせる そうだ　幸せになる呪縛
+              いつからこんなだったっけ
+            </v-card-text>
+            <v-divider></v-divider>
+            <v-card-actions>
+              <v-btn text="この楽曲を選択" variant="text" @click="formPanel = true"></v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-container>
+
+  <v-dialog v-model="formPanel">
+    <v-card class="ma-4">
+      <v-card-title>演習情報の入力</v-card-title>
+      <v-divider></v-divider>
+      <v-card-text>
+        <v-text-field label="グループ番号"></v-text-field>
+        <v-text-field label="制作メンバー"></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn text @click="() => $router.push('/edit')">次へ</v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+
   <v-dialog v-model="playPanel">
     <PlayMoviePanel />
   </v-dialog>
@@ -31,6 +72,7 @@ interface MovieObject {
 }
 const movies = ref<MovieObject[]>([])
 const playPanel = ref(false)
+const formPanel = ref(false)
 
 // @/public/userdata/sample.jsonから、ムービーデータを取得してmoviesにセット
 onMounted(() => {
