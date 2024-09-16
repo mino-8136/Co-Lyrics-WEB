6ea7f6056d3820f248ab17e88be9abec350e73f9
min <67621261+mino-8136@users.noreply.github.com>
2024-09-17 01:54:01 +0900

    ランダム拡大回転の追加

--- src/assets/effects/animation.ts
+++ src/assets/effects/animation.ts
@@ -53,36 +53,6 @@ export const animationList: Animation[] = [
     }
   },
   {
-    name: 'フェードイン',
-    params: { time: 10, interval: 1, out: false },
-    description:
-      '[時間]で指定したフレーム数で、不透明度が0→100に変化します。[退場]をONにするとオブジェクト終了地点を基準としたフェードアウトになります。フェードエフェクトを2個重ねるとフェードイン・フェードアウトができます。',
-    tag: ['登場', '退場'],
-    parameters: {
-      time: { name: '時間(f)', type: UIType.slider, min: 0, max: 60 },
-      interval: { name: '間隔(f)', type: UIType.slider, min: 0, max: 20 },
-      out: { name: '退場', type: UIType.checkbox }
-    },
-    applyEffect: (
-      inform: Inform,
-      baseObject: Transform,
-      params: { [key: string]: any }
-    ): Transform => {
-      const transform = new Transform()
-
-      // out=trueの場合は1 → 0 に、 falseの場合は0 → 1 に変化する
-      const progress = params.out
-        ? (inform.end -
-            inform.currentFrame -
-            (inform.totalIndex - 1 - inform.index) * params.interval) /
-          params.time
-        : (inform.currentFrame - inform.start - inform.index * params.interval) / params.time
-      transform.opacity = baseObject.opacity * clClamp(0, 1, progress)
-
-      return transform
-    }
-  },
-  {
     name: '明滅登場',
     params: { time: 5, interval: 1, out: false },
     description: '文字を一文字ずつ表示します。[退場]をONにすると明滅して消えるようになります。',
@@ -108,6 +78,36 @@ export const animationList: Animation[] = [
       else if (progressFrame < params.time && Math.round(progressFrame) % 2 === 0) {
         transform.opacity = 0
       }
+
+      return transform
+    }
+  },
+  {
+    name: 'フェードイン',
+    params: { time: 10, interval: 1, out: false },
+    description:
+      '[時間]で指定したフレーム数で、不透明度が0→100に変化します。[退場]をONにするとオブジェクト終了地点を基準としたフェードアウトになります。フェードエフェクトを2個重ねるとフェードイン・フェードアウトができます。',
+    tag: ['登場', '退場'],
+    parameters: {
+      time: { name: '時間(f)', type: UIType.slider, min: 0, max: 60 },
+      interval: { name: '間隔(f)', type: UIType.slider, min: 0, max: 20 },
+      out: { name: '退場', type: UIType.checkbox }
+    },
+    applyEffect: (
+      inform: Inform,
+      baseObject: Transform,
+      params: { [key: string]: any }
+    ): Transform => {
+      const transform = new Transform()
+
+      // out=trueの場合は1 → 0 に、 falseの場合は0 → 1 に変化する
+      const progress = params.out
+        ? (inform.end -
+            inform.currentFrame -
+            (inform.totalIndex - 1 - inform.index) * params.interval) /
+          params.time
+        : (inform.currentFrame - inform.start - inform.index * params.interval) / params.time
+      transform.opacity = baseObject.opacity * clClamp(0, 1, progress)
 
       return transform
     }
@@ -195,7 +195,6 @@ export const animationList: Animation[] = [
     params: {
       X: new KeyframeSettings([{ frame: 0, value: 0, id: generateUniqueId() }]), // キーフレーム対応
       Y: new KeyframeSettings([{ frame: 0, value: 150, id: generateUniqueId() }]), // キーフレーム対応
-      Scale: new KeyframeSettings([{ frame: 0, value: 100, id: generateUniqueId() }]), // キーフレーム対応
       Time: new KeyframeSettings([{ frame: 0, value: 0, id: generateUniqueId() }]),
       Seed: 100,
       Side: true
@@ -228,19 +227,25 @@ export const animationList: Animation[] = [
       }
       return transform
     }
-  }
-]
-
-/*
+  },
   {
-    name: 'フェードアウト',
-    params: { time: 10, interval: 1 },
+    name: 'ランダム拡大回転',
+    params: {
+      scale: new KeyframeSettings([{ frame: 0, value: 150, id: generateUniqueId() }]), // キーフレーム対応
+      angle: new KeyframeSettings([{ frame: 0, value: 0, id: generateUniqueId() }]), // キーフレーム対応
+      Time: new KeyframeSettings([{ frame: 0, value: 0, id: generateUniqueId() }]),
+      Seed: 100,
+      Side: true
+    },
     description:
-      '[時間]で指定したフレーム数で、オブジェクト終了地点を基準として不透明度が100→0に変化します。間隔を-1にすると逆順で消失します。',
-    tag: ['消失'],
+      'オブジェクトをランダムに拡大・回転させます。[ゆらぎ]を大きくするとゆっくり動きます。',
+    tag: ['配置'],
     parameters: {
-      time: { name: '時間(f)', type: UIType.slider, min: 0, max: 60 },
-      interval: { name: '間隔(f)', type: UIType.slider, min: 0, max: 20 }
+      scale: { name: '拡大率', type: UIType.keyframe, min: 0, max: 500 },
+      angle: { name: '回転角', type: UIType.keyframe, min: -500, max: 500 },
+      Time: { name: 'ゆらぎ', type: UIType.keyframe, min: 0, max: 100 },
+      Seed: { name: 'シード値', type: UIType.slider, min: 0, max: 1000 },
+      Side: { name: '両側', type: UIType.checkbox }
     },
     applyEffect: (
       inform: Inform,
@@ -249,15 +254,17 @@ export const animationList: Animation[] = [
     ): Transform => {
       const transform = new Transform()
 
-      // 1 → 0 に変化する
-      const progress =
-        (inform.end -
-          inform.currentFrame -
-          (inform.totalIndex - 1 - inform.index) * params.interval) /
-        params.time
-      transform.opacity = baseObject.opacity * clClamp(0, 1, progress)
+      const lerpScale = lerpValue(params.scale.keyframes, inform.start, inform.currentFrame)
+      const lerpAngle = lerpValue(params.angle.keyframes, inform.start, inform.currentFrame)
+      const lerpTime = lerpValue(params.Time.keyframes, inform.start, inform.currentFrame)
 
+      if (inform.p5) {
+        const side = params.Side ? 0.5 : 0
+        inform.p5.noiseSeed(params.Seed + inform.index)
+        transform.scale = 100 + (inform.p5.noise(10000 + inform.index + lerpTime / 100) - side) * (lerpScale-100)
+        transform.angle = (inform.p5.noise(10000 + inform.index + lerpTime / 100) - side) * lerpAngle
+      }
       return transform
     }
-  },
-  */
+  }
+]
